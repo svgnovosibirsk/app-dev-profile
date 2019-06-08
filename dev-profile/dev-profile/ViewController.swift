6f9b734a433c91134620669c39e62f704c80b648
Sergey Golovin <sergeygolovin@Mac-mini-Sergey.local>
2019-06-08 09:12:08 +0700

    initial xcode project

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Sergey Golovin on 08.06.2019.
+//  Copyright © 2019 GoldenWindGames LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
